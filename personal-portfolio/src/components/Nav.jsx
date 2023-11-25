d8d7ef3af54e1d537f3c232637408b0e502de43f
Kay Tang <kay@Kays-Laptop.local>
2023-11-25 13:57:16 +1300

    style: refactored navbar component and updated css - Hover effect when hovering over a section in nav bar. - Encapsulated a section component -> removed repeated code.

--- personal-portfolio/src/components/Nav.jsx
+++ personal-portfolio/src/components/Nav.jsx
@@ -2,50 +2,36 @@ import styles from './nav.module.css'
 
 const Nav = ({showNav, home, about, projects, cvTranscript, contact}) => {
 
-  function scrollToMySection(ref) {
-    ref.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
-  }
-
   const navStyle = {
     transform: showNav ? 'translateY(0)' : 'translateY(-100%)',
     transition: 'transform 0.3s ease'
   };
 
   return (
-    <>
-      <div style={navStyle} className={styles.navbar}>
-        <div className={styles.clickables}>
-          <span
-            onClick={()=>scrollToMySection(home)}
-            className={styles.scrollTo}>
-            home
-          </span>
-          <span
-            onClick={()=>scrollToMySection(about)}
-            className={styles.scrollTo}>
-            about
-          </span>
-          <span
-            onClick={()=>scrollToMySection(projects)}
-            className={styles.scrollTo}>
-            projects
-          </span>
-          <span
-            onClick={()=>scrollToMySection(cvTranscript)}
-            className={styles.scrollTo}>
-            CV & Transcript
-          </span>
-          <span
-            onClick={()=>scrollToMySection(contact)}
-            className={styles.scrollTo}>
-            contact
-          </span>
-        </div>
+    <div style={navStyle} className={styles.navbar}>
+      <div className={styles.clickables}>
+        <Section section={home} name='Home'/>
+        <Section section={about} name='About'/>
+        <Section section={projects} name='Projects'/>
+        <Section section={cvTranscript} name='CV & Transcript'/>
+        <Section section={contact} name='Contact'/>
       </div>
-    </>
+    </div>
   )
 }
 
+const Section = ({section, name}) => {
 
+  function scrollToMySection(ref) {
+    ref.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
+  }
+  return (
+    <span
+      onClick={()=>scrollToMySection(section)}
+      className={styles.scrollTo}>
+      {name}
+    </span>
+  )
+}
 
 export default Nav
