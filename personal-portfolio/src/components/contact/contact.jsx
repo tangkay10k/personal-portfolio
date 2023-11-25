179882ea86bff9608064d50a99ffcf6a7a7cb5db
Kay Tang <kay@Kays-Laptop.local>
2023-11-25 22:41:05 +1300

    feat: implemented form functionality - Submit form using web3Forms API

--- personal-portfolio/src/components/contact/contact.jsx
+++ personal-portfolio/src/components/contact/contact.jsx
@@ -1,18 +1,6 @@
 import styles from './contact.module.css'
-import { appendData } from './submit.js'
 
 const Contact = ({ contact }) => {
-  function handleFormSubmit(event) {
-    event.preventDefault()
-    // Assuming you have a form with inputs having IDs 'name', 'email', 'message'
-    const name = document.getElementById('name').value
-    const email = document.getElementById('email').value
-    const message = document.getElementById('message').value
-
-    // Call appendData with the OAuth authentication and form data
-    appendData(auth, name, email, message)
-  }
-
   return (
     <div
       ref={contact}
@@ -20,12 +8,27 @@ const Contact = ({ contact }) => {
       className={styles.contactContainer}
     >
       <h2>Contact Me</h2>
-      <form onSubmit={handleFormSubmit} className={styles.formContainer}>
-        <input type="text" placeholder="Your Name" id="name" />
-        <input type="text" placeholder="Your Email" id="email" />
-        <textarea type="text" placeholder="Your Message" id="message" />
+      <form
+        action="https://api.web3forms.com/submit"
+        method="POST"
+        className={styles.formContainer}
+      >
+        <input
+          type="hidden"
+          name="access_key"
+          value="da99f9da-53b8-4642-ac9a-cec38e66b959"
+        ></input>
+        <input type="text" placeholder="Your Name" name="name" required />
+        <input type="email" placeholder="Your Email" name="email" required />
+        <textarea
+          type="text"
+          placeholder="Your Message"
+          name="message"
+          required
+        />
         <button className={styles.button}>Send</button>
       </form>
+      <script src="https://web3forms.com/client/script.js" async defer></script>
     </div>
   )
 }
