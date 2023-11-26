67091e0dab279b81bf1d95a92b1b0ecb781eac7c
Kay Tang <kay@Kays-Laptop.local>
2023-11-26 13:35:16 +1300

    style: Updated mobile phone view of site. - Made nav bar on top of all elements

--- personal-portfolio/src/components/about/aboutme.js
+++ personal-portfolio/src/components/about/aboutme.js
@@ -13,11 +13,14 @@ I am currently learning fullstack development,
 which I am enjoying a lot; learning how the front and back end is integrated is really interesting to me,
 and I love the feeling when my imagination of a website comes to fruition.`
 
-const skills = `Languages: Java, JavaScript, TypeScript, Python, C
+const skills = `Languages: 
+Java, JavaScript, TypeScript, Python, C
 
-Tools: Docker, Firestore, MongoDB, Google Cloud Run
+Tools: 
+Docker, Firestore, MongoDB, Google Cloud Run
 
-Other: Photoshop, Sony Vegas Pro`
+Other: 
+Photoshop, Sony Vegas Pro`
 
 const experence = `2023
 NZPMC Summer Internship 
