02d09a2d16423825715620fef195a3fa59493a1f
Kay Tang <126426690+ktan185@users.noreply.github.com>
2025-08-02 19:02:06 +1200

    Serve static fe in backend

--- server/server.js
+++ server/server.js
@@ -4,11 +4,19 @@ require("dotenv").config();
 const cors = require("cors");
 const PORT = process.env.PORT || 8080;
 const contactRoutes = require("./routes/contactRoutes");
+const path = require("path");
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/contact", contactRoutes);
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+// Fallback to index.html for SPA routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
 // Start the server
 app.listen(PORT, () => {
