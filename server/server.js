41aeebe866cee66c2753bdfea14636353478de4b
Kay Tang <126426690+ktan185@users.noreply.github.com>
2025-08-02 20:03:05 +1200

    hot fix, use json

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+app.use(express.json());
 
 app.use("/api/contact", require("./routes/contactRoutes"));
 
